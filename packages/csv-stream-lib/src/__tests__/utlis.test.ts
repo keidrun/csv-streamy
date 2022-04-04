76c699165a9721928f1d7540d80f1924b8cac22e
keidrun <kssk96@gmail.com>
2022-04-05 07:32:32 +0900

    feat(lib): add splitByNewLine function

--- packages/csv-stream-lib/src/__tests__/utlis.test.ts
+++ packages/csv-stream-lib/src/__tests__/utlis.test.ts
@@ -1,4 +1,4 @@
-import { filename, dirname } from '../utils.js'
+import { filename, dirname, splitByNewline } from '../utils.js'
 
 describe('utils', () => {
   describe('filename', () => {
@@ -9,6 +9,83 @@ describe('utils', () => {
   describe('dirname', () => {
     test('should return a directory path', () => {
       expect(dirname({ url: 'file:///dir/file.txt' })).toBe('/dir')
+    })
+  })
+  describe('splitByNewline', () => {
+    test('should split data by new line', () => {
+      const data = `header[1],header[2],header[3],header[4],header[5]
+item[1][1],item[1][2],item[1][3],item[1][4],item[1][5]
+item[2][1],item[2][2],item[2][3],item[2][4],item[2][5]
+item[3][1],item[3][2],item[3][3],item[3][4],item[3][5]`
+
+      const lines = splitByNewline(data)
+
+      expect(lines.length).toBe(4)
+    })
+    test('should split data that has final new line by new line', () => {
+      const data = `header[1],header[2],header[3],header[4],header[5]
+item[1][1],item[1][2],item[1][3],item[1][4],item[1][5]
+item[2][1],item[2][2],item[2][3],item[2][4],item[2][5]
+item[3][1],item[3][2],item[3][3],item[3][4],item[3][5]
+`
+
+      const lines = splitByNewline(data)
+
+      expect(lines.length).toBe(5)
+    })
+    test('should split data that has first new line by new line', () => {
+      const data = `
+header[1],header[2],header[3],header[4],header[5]
+item[1][1],item[1][2],item[1][3],item[1][4],item[1][5]
+item[2][1],item[2][2],item[2][3],item[2][4],item[2][5]
+item[3][1],item[3][2],item[3][3],item[3][4],item[3][5]`
+
+      const lines = splitByNewline(data)
+
+      expect(lines.length).toBe(4)
+    })
+    test('should split double-quoted data by new line', () => {
+      const data = `"header[1]","header[2]","header[3]","header[4]","header[5]"
+"item[1][1]","item[1][2]","item[1][3]","item[1][4]","item[1][5]"
+"item[2][1]","item[2][2]","item[2][3]","item[2][4]","item[2][5]"
+"item[3][1]","item[3][2]","item[3][3]","item[3][4]","item[3][5]"`
+
+      const lines = splitByNewline(data, { hasDoubleQuotes: true })
+
+      expect(lines.length).toBe(4)
+    })
+    test('should split double-quoted data that has final new line by new line', () => {
+      const data = `"header[1]","header[2]","header[3]","header[4]","header[5]"
+"item[1][1]","item[1][2]","item[1][3]","item[1][4]","item[1][5]"
+"item[2][1]","item[2][2]","item[2][3]","item[2][4]","item[2][5]"
+"item[3][1]","item[3][2]","item[3][3]","item[3][4]","item[3][5]"
+`
+
+      const lines = splitByNewline(data, { hasDoubleQuotes: true })
+
+      expect(lines.length).toBe(5)
+    })
+    test('should split double-quoted data that has first new line by new line', () => {
+      const data = `
+"header[1]","header[2]","header[3]","header[4]","header[5]"
+"item[1][1]","item[1][2]","item[1][3]","item[1][4]","item[1][5]"
+"item[2][1]","item[2][2]","item[2][3]","item[2][4]","item[2][5]"
+"item[3][1]","item[3][2]","item[3][3]","item[3][4]","item[3][5]"`
+
+      const lines = splitByNewline(data, { hasDoubleQuotes: true })
+
+      expect(lines.length).toBe(4)
+    })
+    test('should split double-quoted data that has escape characters new line by new line', () => {
+      const data = `"header[1]","header[2]","header[3]","header[4]","header[5]"
+",item[1][1]","item,[1][2]","item[1],[3]","item[1][4],","item[1][5],"
+"""item[2][1]","item""[2][2]","item[2]""[3]","item[2][4]""","item[2][5]"""
+"\\"item[3][1]","item\\"[3][2]","item[3]\\"[3]","item[3][4]\\"","item[3][5]"
+"item[4][1]","item[4][2]","item[4][3]","item[4][4]","item[4][5]"`
+
+      const lines = splitByNewline(data, { hasDoubleQuotes: true })
+
+      expect(lines.length).toBe(5)
     })
   })
 })
--- packages/csv-stream-lib/src/utils.ts
+++ packages/csv-stream-lib/src/utils.ts
@@ -8,3 +8,57 @@ export function filename(importMeta: { url: string }): string {
 export function dirname(importMeta: { url: string }): string {
   return path.dirname(filename(importMeta))
 }
+
+const escapeCharsInCsv = ['"', '\\']
+
+export function splitByNewline(data: string, options?: { hasDoubleQuotes: boolean }): string[] {
+  const hasDoubleQuotes = options?.hasDoubleQuotes || false
+
+  if (!hasDoubleQuotes) return data.trimStart().split('\n')
+
+  const hasEndOfLine = (line: string): boolean => {
+    if (line.length <= 3) return false
+
+    if (!escapeCharsInCsv.includes(line.slice(-3, -2)) && line.slice(-2, -1) === '"' && line.slice(-1) === '\n') {
+      return true
+    } else if (line.slice(-2, -1) === '"' && line.slice(-1) === '\n') {
+      const reversed = line.slice(0, -2).split('').reverse().join('')
+      let escapeCount = 0
+      for (const char of reversed) {
+        if (escapeCharsInCsv.includes(char)) escapeCount++
+        break
+      }
+      if (escapeCount % 2 !== 0) {
+        return true
+      }
+    }
+
+    return false
+  }
+
+  const lines = []
+  let line = ''
+  for (const char of data.trimStart()) {
+    if (line.length <= 3) {
+      line += char
+      continue
+    }
+
+    if (hasEndOfLine(line)) {
+      lines.push(line.slice(0, -1))
+      line = ''
+    }
+
+    line += char
+  }
+
+  // check last characters because `for...of` statement truncates the final new line character
+  if (hasEndOfLine(line)) {
+    lines.push(line.slice(0, -1))
+    line = ''
+  }
+
+  lines.push(line)
+
+  return lines
+}

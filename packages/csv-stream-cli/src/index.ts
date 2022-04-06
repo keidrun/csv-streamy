a4d6b1f02bf51a778514f614750381ef3b75c00c
keidrun <kssk96@gmail.com>
2022-04-06 17:14:08 +0900

    chore: change exports in package.json

--- packages/csv-stream-cli/src/index.ts
+++ packages/csv-stream-cli/src/index.ts
@@ -1,3 +1,12 @@
-import { hello } from './utils.js'
+import * as fs from 'fs'
+import * as path from 'path'
+import { createCsvParser } from '@csv-stream/lib'
 
-export { hello }
+const reader = fs.createReadStream(path.resolve(__dirname, 'your.csv'))
+const parser = createCsvParser({ hasHeaders: true, hasDoubleQuotes: true })
+
+reader
+  .pipe(parser)
+  .on('error', (error: Error) => console.log(error))
+  .on('data', (row) => console.log(row))
+  .on('end', () => console.log('Done!'))

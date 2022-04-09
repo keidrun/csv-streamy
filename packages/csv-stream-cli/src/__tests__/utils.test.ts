70a5d41a0d0557d45e1feb60e22ab2cbb06a3b16
keidrun <kssk96@gmail.com>
2022-04-10 04:37:02 +0900

    feat(cli): add toInt function

--- packages/csv-stream-cli/src/__tests__/utils.test.ts
+++ packages/csv-stream-cli/src/__tests__/utils.test.ts
@@ -1,9 +1,42 @@
-import { hello } from '../utils.js'
+import { toInt } from '../utils.js'
 
 describe('utils', () => {
-  describe('hello', () => {
-    test('should return message', () => {
-      expect(hello()).toBe('Hello')
+  describe('toInt', () => {
+    test('should return a number when a size is integer', () => {
+      expect(toInt('39')).toBe(39)
+    })
+    test('should return a ceiled number when a size is float', () => {
+      expect(toInt('39.1')).toBe(40)
+    })
+    test('should return a NaN when a size is string', () => {
+      expect(toInt('stuff1234')).toBe(NaN)
+    })
+    test('should return a NaN when a size is string with unit', () => {
+      expect(toInt('stuff1234K')).toBe(NaN)
+    })
+    test('should return  a number * K unit', () => {
+      expect(toInt('3K')).toBe(3 * 1024)
+    })
+    test('should return  a number * M unit', () => {
+      expect(toInt('5M')).toBe(5 * 1024 * 1024)
+    })
+    test('should return  a number * G unit', () => {
+      expect(toInt('12G')).toBe(12 * 1024 * 1024 * 1024)
+    })
+    test('should return  a number * T unit', () => {
+      expect(toInt('1T')).toBe(1 * 1024 * 1024 * 1024 * 1024)
+    })
+    test('should return  a number * P unit', () => {
+      expect(toInt('20P')).toBe(20 * 1024 * 1024 * 1024 * 1024 * 1024)
+    })
+    test('should return  a number * E unit', () => {
+      expect(toInt('7E')).toBe(7 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024)
+    })
+    test('should return  a number * Z unit', () => {
+      expect(toInt('99Z')).toBe(99 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024)
+    })
+    test('should return  a number * Y unit', () => {
+      expect(toInt('8Y')).toBe(8 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024)
     })
   })
 })
--- packages/csv-stream-cli/src/utils.ts
+++ packages/csv-stream-cli/src/utils.ts
@@ -1,3 +1,27 @@
-export function hello(): string {
-  return 'Hello'
+const SIZE_UNIT: {
+  [key: string]: number
+} = {
+  K: 1024,
+  M: 1024 * 1024,
+  G: 1024 * 1024 * 1024,
+  T: 1024 * 1024 * 1024 * 1024,
+  P: 1024 * 1024 * 1024 * 1024 * 1024,
+  E: 1024 * 1024 * 1024 * 1024 * 1024 * 1024,
+  Z: 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024,
+  Y: 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024,
+}
+
+export function toInt(size: string): number {
+  const numStr = size.slice(0, -1)
+  const num = !Number.isNaN(Number(numStr)) ? Math.ceil(Number(numStr)) : NaN
+  const unitKey = size.slice(-1)
+
+  if (!Object.keys(SIZE_UNIT).includes(unitKey)) {
+    if (!Number.isNaN(Number(size))) {
+      return Math.ceil(Number(size))
+    }
+    return NaN
+  }
+
+  return num * Number(SIZE_UNIT[unitKey])
 }
